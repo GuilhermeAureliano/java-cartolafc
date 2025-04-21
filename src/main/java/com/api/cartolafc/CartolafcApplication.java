69de821173c899998799dd4ed4258666f88f61ea
Guilherme <guilherme.farias@ccc.ufcg.edu.br>
2025-04-21 18:47:47 -0300

    chore: configuração inicial do projeto

---
+++ src/main/java/com/api/cartolafc/CartolafcApplication.java
@@ -0,0 +1,13 @@
+package com.api.cartolafc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartolafcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartolafcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/cartolafc/CartolafcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.cartolafc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartolafcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
